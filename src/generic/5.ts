5663bdd36e62a89a7b01d700ab1dd13ca5b9667e
Serhii Repinskyi <115186821+serhiirepinskyi@users.noreply.github.com>
2024-07-15 23:42:54 +0300

    Complete "generic" 5 task

--- src/generic/5.ts
+++ src/generic/5.ts
@@ -3,9 +3,20 @@
   Використовуйте generics, щоб цей інтерфейс міг працювати з будь-якими типами ключів та значень.
 */
 
-interface KeyValuePair {
-  key;
-  value;
+interface KeyValuePair<TKey, TValue> {
+  key: TKey;
+  value: TValue;
 }
+
+// Examples
+const stringNumberPair: KeyValuePair<string, number> = {
+  key: "age",
+  value: 30,
+};
+
+const numberBooleanPair: KeyValuePair<number, boolean> = {
+  key: 1,
+  value: true,
+};
 
 export {};
